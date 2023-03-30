96a2fb2d526452b199b68dcabc5633f53c12c1bc
Shreyas Jenner <shreyasjenner4@gmail.com>
2023-03-30 21:56:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import subprocess
 def play_sound(file_path):
     subprocess.call(["paplay",file_path])
 
-file_path = "/home/god/code/pomodoro/pomo/al1.wav"
+file_path = "file_path_for_sound_file"
 play_sound(file_path)
