fa0429840add2982d44ab7458cd8211d9470c945
ShreyasJenner <shreyasjenner4@gmail.com>
2023-04-06 16:45:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
+#this program is used to play the sound file which acts as an alarm
+#mp3 or .wav files may be used
+#sample .wav files is provided
+
 import os
 import subprocess
+
 
 def play_sound(file_path):
     subprocess.call(["paplay",file_path])
 
+#place the filepath for the sound file you want as your alarm
 file_path = "file_path_for_sound_file"
 play_sound(file_path)
